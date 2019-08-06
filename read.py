77d4cd35e85c5bde481fb6e5165da3e3bf8325c9
Peter Chen <peter90079@gmail.com>
2019-08-07 00:24:38 +0800

    final edition

--- read.py
+++ read.py
@@ -1,9 +1,13 @@
 data = []
+count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
-
-print(len(data))
-print(data[0])
-print('-----------')
-print(data[1])
+		count += 1
+		if count % 1000 ==0:
+			print(len(data))
+print('檔案讀取玩了,總共有', len(data),'比資料')
+sum_len = 0
+for d in data:
+	sum_len = sum_len +len(d)
+print('留言的平均長度爲', sum_len/len(data))
