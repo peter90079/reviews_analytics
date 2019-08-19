b9b4e07545f929a34d6e1132f1d3e75204358f5a
Peter Chen <peter90079@gmail.com>
2019-08-19 23:55:30 +0800

    count

--- read.py
+++ read.py
@@ -7,6 +7,7 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 ==0:
 			print(len(data))
 print('檔案讀取玩了,總共有', len(data),'筆資料')
+
 sum_len = 0
 for d in data:
 	sum_len = sum_len +len(d)
@@ -23,3 +24,24 @@ good = [d for d in data if 'good' in d]
 
 print('一共有',len(good), '筆留言提到good')
 print(good[0])
+
+wc = {} #word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 #新增新的key進wc字典
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+while True:
+	word = input('請問你想查什麼字')
+	if word == 'q':
+	 	break
+	if word in wc:
+		print(word, '出現過的次數爲：', wc[word])
+	else:
+		print('無此字喔')
+print('感謝使用')
